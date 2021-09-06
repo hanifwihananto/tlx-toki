50f63a18fe61e358729b258ac114baec500b9939
Hanif <hanifabid19@gmail.com>
2021-09-06 13:15:13 +0700

    initial commit

---
+++ Pemrograman Dasar/03. Ekspresi dan Masukan/D. Bebek Untuk Teman.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b, c, d;
+	cin >> a >> b;
+	c = a / b;
+	d = a % b;
+	cout << "masing-masing " << c << "\n";
+	cout << "bersisa " << d << endl;
+	return 0;
+}
---
+++ Pemrograman Dasar/03. Ekspresi dan Masukan/E. Luas Segitiga.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float A, T, l;
+	cin >> A >> T;
+	l = (A * T) / 2;
+	printf("%.2f\n", l);
+	return 0;
+}
